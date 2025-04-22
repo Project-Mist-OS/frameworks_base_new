18baa19df0038f18e029d79b8f3cb57c10e228a2
Fabián Kozynski <kozynski@google.com>
2025-04-22 15:40:25 -0400

    Update heights when QQS height changes
    When the height of QQS changes (usually due to media changes), trigger a
    callback in QuickSettingsControllerImpl to trigger update on the
    heights.
    
    With this, we don't need to react specially in the media visibility
    callback.
    
    Test: manual, add and remove media, observe notif stack
    Test: atest QuickSettingsControllerImpl
    Flag: com.android.systemui.qs_ui_refactor_compose_fragment
    Fixes: 409702452
    Change-Id: I41f12d4082d0fea53a767b3bc7cb438650cd6797
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
@@ -316,6 +316,13 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
     private FlingQsWithoutClickListener mFlingQsWithoutClickListener;
     private ExpansionHeightSetToMaxListener mExpansionHeightSetToMaxListener;
     private final QS.HeightListener mQsHeightListener = this::onHeightChanged;
+    private final QS.QqsHeightListener mQqsHeightListener = new QS.QqsHeightListener() {
+        @Override
+        public void onQqsHeightChanged() {
+            updateHeightsOnShadeLayoutChange();
+            mPanelViewControllerLazy.get().positionClockAndNotifications();
+        }
+    };
     private final Runnable mQsCollapseExpandAction = this::collapseOrExpandQs;
     private final QS.ScrollListener mQsScrollListener = this::onScroll;
 
@@ -2236,7 +2243,6 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
     /** */
     public final class QsFragmentListener implements FragmentHostManager.FragmentListener,
             TunerService.Tunable {
-        private boolean mPreviouslyVisibleMedia = false;
 
         /** */
         @Override
@@ -2248,6 +2254,9 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
             mQs.setOverscrolling(mStackScrollerOverscrolling);
             mQs.setInSplitShade(mSplitShadeEnabled);
             mQs.setIsNotificationPanelFullWidth(mIsFullWidth);
+            if (QSComposeFragment.isEnabled()) {
+                mQs.setQqsHeightListener(mQqsHeightListener);
+            }
 
             // recompute internal state when qspanel height changes
             mQs.getView().addOnLayoutChangeListener(
@@ -2263,12 +2272,7 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
                     setAnimateNextNotificationBounds(
                             StackStateAnimator.ANIMATION_DURATION_STANDARD, 0);
                     mNotificationStackScrollLayoutController.animateNextTopPaddingChange();
-                    if (QSComposeFragment.isEnabled() && mPreviouslyVisibleMedia != visible) {
-                        updateHeightsOnShadeLayoutChange();
-                        mPanelViewControllerLazy.get().positionClockAndNotifications();
-                    }
                 }
-                mPreviouslyVisibleMedia = visible;
             });
             mLockscreenShadeTransitionController.setQS(mQs);
             if (QSComposeFragment.isEnabled()) {
