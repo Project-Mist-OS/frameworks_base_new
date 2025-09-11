0853083eb73b34bcd3d99aa8d33b5df788a3e215
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-09-11 21:25:48 +0800

    [BOOST_FRAMEWORK] system integration
    Change-Id: Ic83259a257405a8666960dd873c06257e9d79025
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -180,6 +180,7 @@ import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.user.domain.interactor.SelectedUserInteractor;
 import com.android.systemui.util.DeviceConfigProxy;
 import com.android.systemui.util.kotlin.JavaAdapter;
+import com.android.systemui.util.NTBoosterController;
 import com.android.systemui.util.settings.SecureSettings;
 import com.android.systemui.util.settings.SystemSettings;
 import com.android.systemui.util.time.SystemClock;
@@ -915,8 +916,9 @@ public class KeyguardViewMediator implements CoreStartable,
             if (targetUserId != mSelectedUserInteractor.getSelectedUserId()) {
                 return;
             }
-            Log.d(TAG, "keyguardDone: " + targetUserId);
-            tryKeyguardDone(targetUserId);
+            NTBoosterController.get().acquireKeyguardGoneAnimationBoost();
+            Log.d(TAG, "keyguardDone");
+            tryKeyguardDone();
         }
 
         @Override
@@ -975,6 +977,7 @@ public class KeyguardViewMediator implements CoreStartable,
                 }
             }
             Trace.endSection();
+            NTBoosterController.get().releaseKeyguardGoneAnimationBoost();
         }
 
         @Override
