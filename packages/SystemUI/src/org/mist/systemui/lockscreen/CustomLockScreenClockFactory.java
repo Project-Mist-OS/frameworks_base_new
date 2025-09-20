08f9033d51abd6729939f6d87aa76f1b9e8f27c8
ruyue <ruyueee@outlook.com>
2025-09-21 00:27:04 +0800

    SystemUI: AviumCustomLockscreen: Add 5 new design custom lockscreens
    Change-Id: I93ed73d610f17be38949a5835237573ccc6ae829

--- packages/SystemUI/src/org/mist/systemui/lockscreen/CustomLockScreenClockFactory.java
+++ packages/SystemUI/src/org/mist/systemui/lockscreen/CustomLockScreenClockFactory.java
@@ -5,6 +5,13 @@ import android.util.Log;
 import org.mist.systemui.lockscreen.type.bigboom.BigBoomClockController;
 import org.mist.systemui.lockscreen.util.CustomLockscreenSettings;
 import org.mist.systemui.lockscreen.type.smallcuteclock.SmallCuteClockController;
+import org.mist.systemui.lockscreen.type.easterncharacters.EasternCharactersClockController;
+import org.mist.systemui.lockscreen.type.texttime.TextTimeClockController;
+import org.mist.systemui.lockscreen.type.thinlongclock.ThinLongClockController;
+import org.mist.systemui.lockscreen.type.moremorethin.MoreMoreThinClockController;
+import org.mist.systemui.lockscreen.type.normaltime.NormalTimeClockController;
+import org.mist.systemui.lockscreen.type.runrunclock.RunrunClockController;
+import org.mist.systemui.lockscreen.type.classicclock.ClassicClockController;
 
 public class CustomLockScreenClockFactory {
 
@@ -22,6 +29,20 @@ public class CustomLockScreenClockFactory {
                 return new BigBoomClockController();
             case 2:
                 return new SmallCuteClockController();
+            case 3:
+                return new EasternCharactersClockController();
+            case 4:
+                return new TextTimeClockController();
+            case 5:
+                return new ThinLongClockController();
+            case 6:
+                return new MoreMoreThinClockController();
+            case 8:
+                return new NormalTimeClockController();
+            case 9:
+                return new RunrunClockController();
+            case 10:
+                return new ClassicClockController();
             default:
                 Log.w(TAG, "Unknown clock type: " + clockType);
                 return null;
