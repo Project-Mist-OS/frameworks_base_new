AviumUI/android_frameworks_base

AviumLockscreen: New Music Lockscreen
Change-Id: I4a8176f62e66317861adca6023f81c9f529310f0

packages/SystemUI/src/org/avium/systemui/lockscreen/CustomLockScreenClockFactory.java
@@ -19,6 +19,7 @@ import org.avium.systemui.lockscreen.type.guoguoclock.GuoguoClockController4;
 import org.avium.systemui.lockscreen.type.guoguoclock.GuoguoClockController5;
 import org.avium.systemui.lockscreen.type.guoguoclock.GuoguoClockController6;
 import org.avium.systemui.lockscreen.type.guoguoclock.GuoguoClockController7;
+import org.avium.systemui.lockscreen.type.mediablur.MediaBlurClockController;
 
 public class CustomLockScreenClockFactory {
 
@@ -69,6 +70,9 @@ public class CustomLockScreenClockFactory {
 
             case 17:
                 return new GuoguoClockController7();
+
+            case 18:
+                return new MediaBlurClockController();
             default:
                 Log.w(TAG, "Unknown clock type: " + clockType);
                 return new TextTimeClockController();

packages/SystemUI/src/org/avium/systemui/lockscreen/type/mediablur/BlurUtils.java
@@ -1,0 +1,95 @@
+package org.avium.systemui.lockscreen.type.mediablur;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.ParcelFileDescriptor;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.app.WallpaperManager;
+import android.util.Log;
+
+import java.io.FileDescriptor;
+import android.graphics.BitmapFactory;
+
+public class BlurUtils {
+
+    private static final String TAG = "BlurUtils";
+
+    public static Bitmap blur(Context context, Bitmap image, float blurRadius) {
+        if (image == null || image.isRecycled()) {
+            return null;
+        }
+
+        float scale = 0.25f; 
+        int width = Math.round(image.getWidth() * scale);
+        int height = Math.round(image.getHeight() * scale);
+        
+        if (width == 0 || height == 0) return null;
+
+        Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, false);
+        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
+
+        RenderScript rs = null;
+        try {
+            rs = RenderScript.create(context);
+            ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+            Allocation inAlloc = Allocation.createFromBitmap(rs, inputBitmap);
+            Allocation outAlloc = Allocation.createFromBitmap(rs, outputBitmap);
+            
+            script.setRadius(blurRadius > 25f ? 25f : blurRadius);
+            
+            script.setInput(inAlloc);
+            script.forEach(outAlloc);
+            outAlloc.copyTo(outputBitmap);
+        } finally {
+            if (rs != null) {
+                rs.destroy();
+            }
+        }
+        
+        inputBitmap.recycle(); 
+
+        return outputBitmap;
+    }
+
+    public static Bitmap getLockscreenWallpaper(Context context) {
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(context);
+        try {
+            ParcelFileDescriptor pfd = wallpaperManager.getWallpaperFile(WallpaperManager.FLAG_LOCK);
+            if (pfd != null) {
+                FileDescriptor fd = pfd.getFileDescriptor();
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap wallpaperBitmap = BitmapFactory.decodeFileDescriptor(fd, null, options);
+                pfd.close();
+                return wallpaperBitmap;
+            }
+        } catch (Exception e) {
+            //ntd
+        }
+
+        try {
+            Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+            if (wallpaperDrawable instanceof BitmapDrawable) {
+                return ((BitmapDrawable) wallpaperDrawable).getBitmap();
+            } else {
+                int width = wallpaperDrawable.getIntrinsicWidth();
+                int height = wallpaperDrawable.getIntrinsicHeight();
+                Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                wallpaperDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                wallpaperDrawable.draw(canvas);
+                return bitmap;
+            }
+        } catch (Exception e) {
+            //ntd
+        }
+
+        return null;
+    }
+}

packages/SystemUI/src/org/avium/systemui/lockscreen/type/mediablur/MusicController.java
@@ -1,0 +1,120 @@
+package org.avium.systemui.lockscreen.type.mediablur;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.MediaMetadata;
+import android.media.session.MediaController;
+import android.media.session.PlaybackState;
+import android.os.Handler;
+import android.os.Looper;
+
+public class MusicController {
+
+    private final MediaController mMediaController;
+    private final MusicStateListener mListener;
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
+    private final Runnable mProgressUpdater;
+
+    public interface MusicStateListener {
+        void onMetadataChanged(String title, String artist, Bitmap albumArt);
+        void onPlaybackStateChanged(boolean isPlaying);
+        void onProgressChanged(long currentPosition, long duration);
+    }
+
+    private final MediaController.Callback mCallback = new MediaController.Callback() {
+        @Override
+        public void onMetadataChanged(MediaMetadata metadata) {
+            updateMetadata();
+        }
+
+        @Override
+        public void onPlaybackStateChanged(PlaybackState state) {
+            updatePlaybackState();
+        }
+    };
+
+    public MusicController(MediaController mediaController, MusicStateListener listener) {
+        if (mediaController == null || listener == null) {
+            throw new IllegalArgumentException("MediaController and MusicStateListener cannot be null");
+        }
+        this.mMediaController = mediaController;
+        this.mListener = listener;
+        this.mMediaController.registerCallback(mCallback);
+
+        this.mProgressUpdater = this::updateProgress;
+
+        initializeState();
+    }
+
+    private void initializeState() {
+        updateMetadata();
+        updatePlaybackState();
+    }
+
+    private void updateMetadata() {
+        MediaMetadata metadata = mMediaController.getMetadata();
+        if (metadata != null) {
+            String title = metadata.getString(MediaMetadata.METADATA_KEY_TITLE);
+            String artist = metadata.getString(MediaMetadata.METADATA_KEY_ARTIST);
+            Bitmap albumArt = metadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART);
+            mListener.onMetadataChanged(title, artist, albumArt);
+        }
+    }
+
+    private void updatePlaybackState() {
+        PlaybackState state = mMediaController.getPlaybackState();
+        if (state != null) {
+            boolean isPlaying = state.getState() == PlaybackState.STATE_PLAYING;
+            mListener.onPlaybackStateChanged(isPlaying);
+            if (isPlaying) {
+                startProgressUpdater();
+            } else {
+                stopProgressUpdater();
+            }
+        }
+    }
+
+    private void updateProgress() {
+        PlaybackState state = mMediaController.getPlaybackState();
+        MediaMetadata metadata = mMediaController.getMetadata();
+        if (state != null && metadata != null) {
+            long currentPosition = state.getPosition();
+            long duration = metadata.getLong(MediaMetadata.METADATA_KEY_DURATION);
+            mListener.onProgressChanged(currentPosition, duration);
+        }
+        mHandler.postDelayed(mProgressUpdater, 500);
+    }
+
+    public void playPause() {
+        PlaybackState state = mMediaController.getPlaybackState();
+        if (state != null) {
+            if (state.getState() == PlaybackState.STATE_PLAYING) {
+                mMediaController.getTransportControls().pause();
+            } else {
+                mMediaController.getTransportControls().play();
+            }
+        }
+    }
+
+    public void nextTrack() {
+        mMediaController.getTransportControls().skipToNext();
+    }
+
+    public void previousTrack() {
+        mMediaController.getTransportControls().skipToPrevious();
+    }
+
+    private void startProgressUpdater() {
+        mHandler.removeCallbacks(mProgressUpdater);
+        mHandler.post(mProgressUpdater);
+    }
+
+    private void stopProgressUpdater() {
+        mHandler.removeCallbacks(mProgressUpdater);
+    }
+
+    public void cleanup() {
+        mMediaController.unregisterCallback(mCallback);
+        stopProgressUpdater();
+    }
+}

Project-Mist-OS/frameworks_base_new

packages/SystemUI/src/org/mist/systemui/lockscreen/CustomLockScreenClockFactory.java
@@ -19,6 +19,7 @@ import org.mist.systemui.lockscreen.type.guoguoclock.GuoguoClockController4;
 import org.mist.systemui.lockscreen.type.guoguoclock.GuoguoClockController5;
 import org.mist.systemui.lockscreen.type.guoguoclock.GuoguoClockController6;
 import org.mist.systemui.lockscreen.type.guoguoclock.GuoguoClockController7;
+import org.mist.systemui.lockscreen.type.mediablur.MediaBlurClockController;
 
 public class CustomLockScreenClockFactory {
 
@@ -69,6 +70,9 @@ public class CustomLockScreenClockFactory {
 
             case 17:
                 return new GuoguoClockController7();
+
+            case 18:
+                return new MediaBlurClockController();
             default:
                 Log.w(TAG, "Unknown clock type: " + clockType);
                 return null;

packages/SystemUI/src/org/mist/systemui/lockscreen/type/mediablur/BlurUtils.java
@@ -1,0 +1,95 @@
+package org.mist.systemui.lockscreen.type.mediablur;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.ParcelFileDescriptor;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.app.WallpaperManager;
+import android.util.Log;
+
+import java.io.FileDescriptor;
+import android.graphics.BitmapFactory;
+
+public class BlurUtils {
+
+    private static final String TAG = "BlurUtils";
+
+    public static Bitmap blur(Context context, Bitmap image, float blurRadius) {
+        if (image == null || image.isRecycled()) {
+            return null;
+        }
+
+        float scale = 0.25f; 
+        int width = Math.round(image.getWidth() * scale);
+        int height = Math.round(image.getHeight() * scale);
+        
+        if (width == 0 || height == 0) return null;
+
+        Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, false);
+        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
+
+        RenderScript rs = null;
+        try {
+            rs = RenderScript.create(context);
+            ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+            Allocation inAlloc = Allocation.createFromBitmap(rs, inputBitmap);
+            Allocation outAlloc = Allocation.createFromBitmap(rs, outputBitmap);
+            
+            script.setRadius(blurRadius > 25f ? 25f : blurRadius);
+            
+            script.setInput(inAlloc);
+            script.forEach(outAlloc);
+            outAlloc.copyTo(outputBitmap);
+        } finally {
+            if (rs != null) {
+                rs.destroy();
+            }
+        }
+        
+        inputBitmap.recycle(); 
+
+        return outputBitmap;
+    }
+
+    public static Bitmap getLockscreenWallpaper(Context context) {
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(context);
+        try {
+            ParcelFileDescriptor pfd = wallpaperManager.getWallpaperFile(WallpaperManager.FLAG_LOCK);
+            if (pfd != null) {
+                FileDescriptor fd = pfd.getFileDescriptor();
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+                Bitmap wallpaperBitmap = BitmapFactory.decodeFileDescriptor(fd, null, options);
+                pfd.close();
+                return wallpaperBitmap;
+            }
+        } catch (Exception e) {
+            //ntd
+        }
+
+        try {
+            Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+            if (wallpaperDrawable instanceof BitmapDrawable) {
+                return ((BitmapDrawable) wallpaperDrawable).getBitmap();
+            } else {
+                int width = wallpaperDrawable.getIntrinsicWidth();
+                int height = wallpaperDrawable.getIntrinsicHeight();
+                Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                wallpaperDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                wallpaperDrawable.draw(canvas);
+                return bitmap;
+            }
+        } catch (Exception e) {
+            //ntd
+        }
+
+        return null;
+    }
+}

packages/SystemUI/src/org/mist/systemui/lockscreen/type/mediablur/MusicController.java
@@ -1,0 +1,120 @@
+package org.mist.systemui.lockscreen.type.mediablur;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.MediaMetadata;
+import android.media.session.MediaController;
+import android.media.session.PlaybackState;
+import android.os.Handler;
+import android.os.Looper;
+
+public class MusicController {
+
+    private final MediaController mMediaController;
+    private final MusicStateListener mListener;
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
+    private final Runnable mProgressUpdater;
+
+    public interface MusicStateListener {
+        void onMetadataChanged(String title, String artist, Bitmap albumArt);
+        void onPlaybackStateChanged(boolean isPlaying);
+        void onProgressChanged(long currentPosition, long duration);
+    }
+
+    private final MediaController.Callback mCallback = new MediaController.Callback() {
+        @Override
+        public void onMetadataChanged(MediaMetadata metadata) {
+            updateMetadata();
+        }
+
+        @Override
+        public void onPlaybackStateChanged(PlaybackState state) {
+            updatePlaybackState();
+        }
+    };
+
+    public MusicController(MediaController mediaController, MusicStateListener listener) {
+        if (mediaController == null || listener == null) {
+            throw new IllegalArgumentException("MediaController and MusicStateListener cannot be null");
+        }
+        this.mMediaController = mediaController;
+        this.mListener = listener;
+        this.mMediaController.registerCallback(mCallback);
+
+        this.mProgressUpdater = this::updateProgress;
+
+        initializeState();
+    }
+
+    private void initializeState() {
+        updateMetadata();
+        updatePlaybackState();
+    }
+
+    private void updateMetadata() {
+        MediaMetadata metadata = mMediaController.getMetadata();
+        if (metadata != null) {
+            String title = metadata.getString(MediaMetadata.METADATA_KEY_TITLE);
+            String artist = metadata.getString(MediaMetadata.METADATA_KEY_ARTIST);
+            Bitmap albumArt = metadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART);
+            mListener.onMetadataChanged(title, artist, albumArt);
+        }
+    }
+
+    private void updatePlaybackState() {
+        PlaybackState state = mMediaController.getPlaybackState();
+        if (state != null) {
+            boolean isPlaying = state.getState() == PlaybackState.STATE_PLAYING;
+            mListener.onPlaybackStateChanged(isPlaying);
+            if (isPlaying) {
+                startProgressUpdater();
+            } else {
+                stopProgressUpdater();
+            }
+        }
+    }
+
+    private void updateProgress() {
+        PlaybackState state = mMediaController.getPlaybackState();
+        MediaMetadata metadata = mMediaController.getMetadata();
+        if (state != null && metadata != null) {
+            long currentPosition = state.getPosition();
+            long duration = metadata.getLong(MediaMetadata.METADATA_KEY_DURATION);
+            mListener.onProgressChanged(currentPosition, duration);
+        }
+        mHandler.postDelayed(mProgressUpdater, 500);
+    }
+
+    public void playPause() {
+        PlaybackState state = mMediaController.getPlaybackState();
+        if (state != null) {
+            if (state.getState() == PlaybackState.STATE_PLAYING) {
+                mMediaController.getTransportControls().pause();
+            } else {
+                mMediaController.getTransportControls().play();
+            }
+        }
+    }
+
+    public void nextTrack() {
+        mMediaController.getTransportControls().skipToNext();
+    }
+
+    public void previousTrack() {
+        mMediaController.getTransportControls().skipToPrevious();
+    }
+
+    private void startProgressUpdater() {
+        mHandler.removeCallbacks(mProgressUpdater);
+        mHandler.post(mProgressUpdater);
+    }
+
+    private void stopProgressUpdater() {
+        mHandler.removeCallbacks(mProgressUpdater);
+    }
+
+    public void cleanup() {
+        mMediaController.unregisterCallback(mCallback);
+        stopProgressUpdater();
+    }
+}